33221fdd0a5616e0934bbccf17ef171771206950
Asanga <asanga.nicholas@yahoo.com>
2019-10-16 17:45:36 +0200

    added db

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express')
+const db = require('./db')
+
+
 const app = express()
+
+
+
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`server is listening on ${port}`))
